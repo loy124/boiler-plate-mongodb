3ba202e28b8609126e6052cae4ff47acdb5b7ac0
LEE ON YOU <dhsdb1541@naver.com>
2020-06-24 15:26:03 +0900

    기초 config 셋팅및 몽고DB연결

--- index.js
+++ index.js
@@ -2,19 +2,41 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+const config = require("./config/key");
+
+//applicatin/x-www-form-urlencoded 를 분석
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json 분석
+app.use(bodyParser.json());
+
 mongoose
-  .connect(
-    "mongodb+srv://loy124:leesj0813@boilerplate-n6k6p.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello world"));
+
+app.post("/register", (req, res) => {
+  // 회원 가입 할때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+  // bodyParser가 req.body를 통해 받아온다
+  const user = new User(req.body);
+
+  //mongodb 저장
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
